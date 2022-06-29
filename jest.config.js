84143194ddf1b02726535c434b3718b226f32b7a
Valentyn Buriakov <vburiakov@intellias.com>
2022-06-29 13:56:27 +0300

    update nx to 13

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  projects: ['<rootDir>/apps/api', '<rootDir>/apps/web'],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };

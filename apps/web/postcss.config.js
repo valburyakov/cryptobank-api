439b839f262e7abcc53689badbd62deb256f19bc
Valentyn Buriakov <vburiakov@intellias.com>
2022-06-29 13:41:55 +0300

    fix tailwind config

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: { config: './tailwind.config.js' },
+    tailwindcss: { config: 'apps/web/tailwind.config.js' },
     autoprefixer: {},
   },
 }
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,10 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 
 module.exports = {
-  purge: createGlobPatternsForDependencies(__dirname),
+  purge: [
+    'apps/web/src/**/*.{js,jsx,ts,tsx}',
+    createGlobPatternsForDependencies(__dirname),
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
